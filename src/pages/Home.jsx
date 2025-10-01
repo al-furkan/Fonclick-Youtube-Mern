b83b236f2ff6028abdd7fefb4fdfc93fd94afd40
al-furkan <furkanf4336@gmail.com>
2025-10-01 09:06:12 +0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 // src/pages/Home.jsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 
 const Home = () => {
   // Example: render 20 cards dynamically
   const cards = Array.from({ length: 20 });
+
+  const {video, setvideo} = useState([]);
+
+  useEffect(()=>{
+    const fatchVideos = async ()=>{
+      const res =await axios.get("/videos/random")
+      setvideo(res.data);
+    }
+    fatchVideos();
+  },[type])
 
   return (
     <div className="flex flex-wrap justify-between gap-4 bg-white dark:bg-gray-900 text-black dark:text-white">
